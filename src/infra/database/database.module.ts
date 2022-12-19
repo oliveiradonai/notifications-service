4c53d36ce0ed0e276cc9ce8f4bceb9817e7f9d0c
oliveiradonai <adonai.fig@gmail.com>
2022-12-18 21:42:23 -0300

    Typescript paths

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { NotificationsRepository } from "src/application/repositories/notifications-repository";
+import { NotificationsRepository } from "@application/repositories/notifications-repository";
 import { PrismaService } from "./prisma/prisma.service";
 import { PrismaNotificationsRepositories } from "./prisma/repositories/prima-notifications-repositories";
 
