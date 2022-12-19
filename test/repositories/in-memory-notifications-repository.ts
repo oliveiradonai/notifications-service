8c0832c16d7f9a41359373c1d64712e21757e745
oliveiradonai <adonai.fig@gmail.com>
2022-12-19 16:21:09 -0300

    Add count recipient notifications

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -14,6 +14,10 @@ export class InMemoryNotificationsRepository implements NotificationsRepository 
         return notification;
     }
 
+    async countManyByRecipientIt(recipientId: string): Promise<number> {
+        return this.notifications.filter(notification => notification.recipientId === recipientId).length;
+    }
+
     async create(notification: Notification) {
         this.notifications.push(notification);
     }
