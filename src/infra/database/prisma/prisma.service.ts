9d1b9ee53ee4b524c34203a6456f64ce2fd8e139
oliveiradonai <adonai.fig@gmail.com>
2022-12-19 17:34:43 -0300

    Implementing HTTP routes

--- src/infra/database/prisma/prisma.service.ts
+++ src/infra/database/prisma/prisma.service.ts
@@ -3,13 +3,19 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
-  }
+    constructor() {
+        super({
+            log: ['query'],
+        })
+    }
 
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
+    async onModuleInit() {
+        await this.$connect();
+    }
+
+    async enableShutdownHooks(app: INestApplication) {
+        this.$on('beforeExit', async () => {
+            await app.close();
+        });
+    }
 }
